2f52f07d2ab9748e65eaacc232a445b151523deb
DTS0 <dreamoftime0@gmail.com>
2018-10-02 18:56:36 +0800

    一个关于switch default的小测试

---
+++ test/test4switch_default.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<stdlib.h>
+using namespace std;
+int main(){
+	int input=1;
+	switch(input){
+		default:
+			cout<<"default"<<endl;break;
+		case 0:
+			cout<<"0"<<endl;break;
+		case 1:
+			cout<<"1"<<endl;break;
+	}
+	return 0;
+}
