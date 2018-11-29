2eecc9025687bce48550e5fd56945cde536ee5f3
DTS0 <dreamoftime0@gmail.com>
2018-11-29 15:46:31 +0800

    增加注释，修改socketClient为可多次发送数据

--- tools/socketTools/socketClient.c
+++ tools/socketTools/socketClient.c
@@ -6,44 +6,51 @@
 #include <unistd.h>
 #include <string.h>
 #include <stdlib.h>
-#include <fcntl.h>
-#include <sys/shm.h>
 
 #define SOCKET_ADDR "127.0.0.1"
 #define SOCKET_PORT  18888
 #define MAXSIZE 1024
 
-int main()
-{
-    int sockfd = socket(AF_INET,SOCK_STREAM, 0);
+int main(){
+    int sockfd;
     struct sockaddr_in sockaddr;
 
     sockaddr.sin_family = AF_INET;
     sockaddr.sin_port = htons(SOCKET_PORT);
     sockaddr.sin_addr.s_addr = inet_addr(SOCKET_ADDR);
 
-    int connfd;
     char sendbuf[MAXSIZE];
     char recvbuf[MAXSIZE];
-    connfd=connect(sockfd, (struct sockaddr *)&sockaddr, sizeof(sockaddr));
-    if (connfd < 0){
-		perror("connect");
-    	exit(EXIT_FAILURE);
+    
+    while(1){ 
+        memset(sendbuf, 0, sizeof(sendbuf));
+        memset(recvbuf, 0, sizeof(recvbuf));
+        sockfd = socket(AF_INET,SOCK_STREAM, 0);
+        if(sockfd<0){
+		    perror("socket");
+		    exit(EXIT_FAILURE);
+	    }
+        printf("sockfd:%d\n",sockfd);
+        if (connect(sockfd, (struct sockaddr *)&sockaddr, sizeof(sockaddr)) < 0){
+		    perror("connect");
+    	    exit(EXIT_FAILURE);
+        }
+
+        //scanf("%s",sendbuf);
+        fgets(sendbuf,MAXSIZE,stdin);//使用fgets可以连控制字符一起读
+
+        //当前逻辑，每次发送后等待接收
+        send(sockfd, sendbuf, strlen(sendbuf),0); ///发送
+        if(strcmp(sendbuf,"exit\n")==0 || strcmp(sendbuf,"exit")==0)
+            break;
+        if(strcmp(sendbuf,"stop\n")==0 || strcmp(sendbuf,"stop")==0)
+            break;
+        recv(sockfd, recvbuf, sizeof(recvbuf),0); ///接收
+        //fputs(recvbuf, stdout);
+        printf("Client:%s\n",sendbuf);
+        printf("Server:%s\n",recvbuf);
+        close(sockfd);
     }
-    memset(sendbuf, 0, sizeof(sendbuf));
-    memset(recvbuf, 0, sizeof(recvbuf));
-
-    scanf("%s",sendbuf);
-
-    send(sockfd, sendbuf, strlen(sendbuf),0); ///发送
-    if(strcmp(sendbuf,"exit\n")==0 || strcmp(sendbuf,"exit")==0)
-        ;//break;
-    recv(sockfd, recvbuf, sizeof(recvbuf),0); ///接收
-    //fputs(recvbuf, stdout);
-    printf("Client:%s\n",sendbuf);
-    printf("Server:%s\n",recvbuf);
-
-    //close(connfd);
-    close(sockfd);
+    
     return 0;
 }
--- tools/socketTools/socketServer.c
+++ tools/socketTools/socketServer.c
@@ -1,10 +1,10 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <arpa/inet.h>
-#include <unistd.h>
-#include <string.h>	//memset
+#include <stdio.h>	//perror,printf
+#include <stdlib.h>	//exit
+#include <sys/socket.h>	//socket,bind,listen,accept,send,recv
+#include <netinet/in.h>	//struct sockaddr_in,htons
+#include <arpa/inet.h>	//inet_addr
+#include <unistd.h>	//close
+#include <string.h>	//memset,strcmp
 
 #define SOCKET_ADDR "127.0.0.1"
 #define SOCKET_PORT 18888
@@ -16,12 +16,12 @@ int main(){
 	uint16_t port=18888;
 	struct sockaddr_in sockaddr;
 
-	sockfd=socket(AF_INET,SOCK_STREAM,0);
+	sockfd=socket(AF_INET,SOCK_STREAM,0);//默认为TCP
 	if(sockfd<0){
 		perror("socket");
 		exit(EXIT_FAILURE);
 	}
-
+	printf("sockfd:%d\n",sockfd);
 	sockaddr.sin_family=AF_INET;
 	sockaddr.sin_port=htons(port);
 	sockaddr.sin_addr.s_addr=inet_addr(SOCKET_ADDR);
@@ -29,7 +29,7 @@ int main(){
 		perror("bind");
 		exit(EXIT_FAILURE);
 	}
-	if(listen(sockfd,MAXSIZE)<0){
+	if(listen(sockfd,MAXSIZE)<0){//最多接受MAXSIZE个连接
 		perror("listen");
 		exit(EXIT_FAILURE);
 	}
@@ -41,23 +41,25 @@ int main(){
 	char buf[MAXSIZE];
 	while(1){
 		connfd=accept(sockfd,(struct sockaddr*)NULL,NULL);
+		printf("connected,sockfd:%d\n",connfd);
 		if(connfd<0){
 			perror("accpet");
 			continue;
 		}
 		memset(buf, 0, sizeof(buf));
-		n=recv(connfd,buf,MAXSIZE,0);
+		n=recv(connfd,buf,MAXSIZE,0);//接收最大MAXSIZE个字符
 		if(strcmp(buf,"exit\n")==0 || strcmp(buf,"exit")==0)
             break;
-		send(connfd,"ok!",3,0);
+		send(connfd,"ok!",3,0);//发送"ok!"
 		printf("%d bytes from client:\n%s\n",n,buf);
 
-		close(connfd);
+		close(connfd);//关闭本连接
 	}
 
-	close(sockfd);
+	printf("closed\n");
+	close(sockfd);//关闭监听端口
 	//shutdown(connfd,SHUT_RDWR);
-
+	//使用shutdown可以达到仅可读或仅可写
 	
 	return 0;
 }
